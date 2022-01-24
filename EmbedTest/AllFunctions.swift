ab73bb6afa9f68d53d55eebde01b6b8080e48713
Satyendra singh <satyendra.bhati@neosoftmail.com>
2022-01-24 13:20:31 +0530

    Initial Commit

---
+++ EmbedTest/AllFunctions.swift
@@ -0,0 +1,14 @@
+//
+//  AllFunctions.swift
+//  EmbedTest
+//
+//  Created by Neosoft on 24/01/22.
+//
+
+import Foundation
+
+class RequiredFunc{
+    static func printHello(){
+    print("Hello")
+  }
+}
---
+++ build/EmbedTest.framework-catalyst.xcarchive/Products/Library/Frameworks/EmbedTest.framework/Versions/A/Headers/EmbedTest.h
@@ -0,0 +1,18 @@
+//
+//  EmbedTest.h
+//  EmbedTest
+//
+//  Created by Neosoft on 24/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmbedTest.
+FOUNDATION_EXPORT double EmbedTestVersionNumber;
+
+//! Project version string for EmbedTest.
+FOUNDATION_EXPORT const unsigned char EmbedTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmbedTest/PublicHeader.h>
+
+
